94a57d1f24ce7872c862ffc902b7472ba673332a
boubelpi <113375173+boubelpi@users.noreply.github.com>
2026-05-11 05:59:28 +0200

    requirements file added; classification commented; some websites, not related to the original article, won't be added

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ article_title = q["metadata"]["title"]
 
 summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 
-search_queries = [article_url]
+search_queries = []
 
 summaries_combined = ""
 
@@ -20,6 +20,8 @@ for _ in range(10):
     summary = summarizer(article_content, do_sample=True)
     search_queries.append(summary[0]["summary_text"])
     summaries_combined += summary[0]["summary_text"] + "\n"
+
+search_queries.append(article_url)
 
 print(summaries_combined)
 
@@ -29,43 +31,45 @@ for query in search_queries:
     search = asyncio.run(miyami.search(query, rerank=True))
     res = search["results"]
     for result in res:
+        if "www.fleet" in result["url"] or "-http" in result["url"] or "/http" in result["url"] or "dictionary" in result["url"]:
+            continue
         unique_urls.add(result["url"])
 
 print(unique_urls)
 
-classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+# classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
 
-for url in unique_urls:
-    q = asyncio.run(miyami.fetch(url))
-    try:
-        content = q["content"]
-        title = q["metadata"]["title"]
-        candidate_text = f"""
-        Original article:
-        title: {article_title}
-        URL: {url}
-        Key claims: {summaries_combined}
-        
-        Candidate source:
-        title: {title}
-        URL: {url}
-        content: {content}
-        """
-        labels = [
-        "the candidate directly references the original article",
-        "the candidate discusses or debates the original article",
-        "the candidate reuses facts or insights from the original article in a broader context",
-        "the candidate summarizes the original article",
-        "the candidate criticizes the original article or JetBrains decision",
-        "the candidate is irrelevant or only weakly related"
-        ]
-        result = classifier(
-        candidate_text,
-        candidate_labels=labels,
-        multi_label=True
-        )
-        print(url)
-        print(result["scores"])
-    except Exception as e:
-        print(url)
-        print(e)
+# for url in unique_urls:
+#    q = asyncio.run(miyami.fetch(url))
+#    try:
+#        content = q["content"]
+#        title = q["metadata"]["title"]
+#        candidate_text = f"""
+#        Original article:
+#        title: {article_title}
+#        URL: {url}
+#        Key claims: {summaries_combined}
+
+#        Candidate source:
+#        title: {title}
+#        URL: {url}
+#        content: {content}
+#        """
+#        labels = [
+#        "the candidate directly references the original article",
+#        "the candidate discusses or debates the original article",
+#        "the candidate reuses facts or insights from the original article in a broader context",
+#        "the candidate summarizes the original article",
+#        "the candidate criticizes the original article or JetBrains decision",
+#        "the candidate is irrelevant or only weakly related"
+#        ]
+#        result = classifier(
+#        candidate_text,
+#        candidate_labels=labels,
+#        multi_label=True
+#        )
+#        print(url)
+#        print(result["scores"])
+#    except Exception as e:
+#        print(url)
+#        print(e)
